f0efb48b187b33bddbd9a7bb20f9640a4b5f3956
miRoox <miroox@outlook.com>
2020-03-02 22:48:23 +0800

    more default methods

--- LazyList/LazyList.m
+++ LazyList/LazyList.m
@@ -18,9 +18,14 @@ LazyList[]:=LazyList[{}]
 
 LazyList/:Normal@LazyList[type_[___]]:=GeneralUtilities`Panic["Unimplemented", "Method `1` is not implemented for type `2`!", Normal, type]
 LazyList/:Length[l:LazyList[_]]:=Length@Normal[l]
+LazyList/:LengthWhile[l:LazyList[_],crit_]:=Length@Normal[TakeWhile[l,crit]]
 LazyList/:Part[l:LazyList[_],i_Integer]:=Part[Normal[l],i]
 LazyList/:First[l:LazyList[_],def_]:=First[Normal[l],def]
 LazyList/:Last[l:LazyList[_],def_]:=Last[Normal[l],def]
+LazyList/:Extract[l:LazyList[_],i_]:=Extract[Normal[l],i]
+LazyList/:AllTrue[l:LazyList[_],test_]:=Normal@Fold[And,test/@l]
+LazyList/:AnyTrue[l:LazyList[_],test_]:=Normal@Fold[Or,test/@l]
+LazyList/:NoneTrue[l:LazyList[_],test_]:=Normal@Fold[And,Not@*test/@l]
 
 End[]; (* `Private` *)
 
