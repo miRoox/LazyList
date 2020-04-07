17c8c701d7b542c04f860b78c067f7bbe6430ea9
miroox <miroox@outlook.com>
2020-04-07 11:28:46 +0800

    check trait name & dependencies

--- LazyList/IteratorBase.m
+++ LazyList/IteratorBase.m
@@ -68,9 +68,9 @@ $IteratorType::usage="$IteratorType is a placeholder for the type of the iterato
 $IteratorData::usage="$IteratorData is a placeholder to access the data of the iterator itself.";
 
 DeclareIterator::typestr="Type name `1` should be a string.";
-DeclareIterator::trait="Unknown trait named `1`.";
 DeclareIterator::satis="Iterator type must satisfy the trait `1`.";
-DeclareIterator::mdeps="The dependencies `2` for `1` is missing.";
+ImplementIterator::trait="Unknown trait named `1`.";
+ImplementIterator::mdeps="The dependencies `2` for `1` is missing.";
 ImplementIterator::require="The method `1` is required.";
 IteratorTraitInfo::trait="Unknown trait named `1`.";
 
@@ -166,20 +166,36 @@ act:DeclareIterator[_String[__], _Association]:=GeneralUtilities`CatchFailureAnd
   registerTypeTemplate[Inactivate[act]];
 ]
 
-ImplementIterator[type:$nonParamatricTypePatt, None|PatternSequence[], methods_]:=GeneralUtilities`CatchFailureAndMessage[
+ImplementIterator[type:$nonParamatricTypePatt, None|PatternSequence[], methods_List]:=GeneralUtilities`CatchFailureAndMessage[
   doImplMethods[type, methods]
 ]
-ImplementIterator[type:$nonParamatricTypePatt, trait_?traitQ, methods_]:=GeneralUtilities`CatchFailureAndMessage[
+ImplementIterator[type:$nonParamatricTypePatt, trait_String, methods_List]:=GeneralUtilities`CatchFailureAndMessage[
+  checkTraitName[trait];
   Internal`InheritedBlock[{traitImplQ},
-    traitImplQ[type, trait]=True;(*todo*)
+    traitImplQ[type, trait]=True;
+    checkTraitDeps[type, trait];
     doImplMethods[type, resolveTraitMethods[trait, methods]]
   ];
   traitImplQ[type, trait]=True;
 ]
-act:ImplementIterator[_String[__], _, _|PatternSequence[]]:=GeneralUtilities`CatchFailureAndMessage[
+act:ImplementIterator[_String[__], trait_String, _List]:=GeneralUtilities`CatchFailureAndMessage[
+  checkTraitName[trait];
   registerTypeTemplate[Inactivate[act]];
 ]
-ImplementIterator[type_, trait_?traitQ]:=ImplementIterator[type, trait, {}]
+act:ImplementIterator[_String[__], None|PatternSequence[], _List]:=GeneralUtilities`CatchFailureAndMessage[
+  registerTypeTemplate[Inactivate[act]];
+]
+ImplementIterator[type_, trait_String]:=ImplementIterator[type, trait, {}]
+
+checkTraitName[trait_]:=If[!traitQ[trait],
+  GeneralUtilities`ThrowFailure[ImplementIterator::trait, trait]
+]
+checkTraitDeps[type_, trait_]:=Block[
+  {mdeps=Select[$traits[[trait, "Deps"]], !traitImplQ[type, #]&]},
+  If[mdeps=!={},
+    GeneralUtilities`ThrowFailure[ImplementIterator::mdeps, trait, mdeps]
+  ]
+]
 
 doImplMethods[type_, methods_]:=GeneralUtilities`BlockProtected[{Iterator},
   Activate[substImplMethods[type]/@methods, SetDelayed]
